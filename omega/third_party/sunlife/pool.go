22438acfe6e49e5d7e97395fe7e836c52c183f79
RainyHallways <78735995+RainyHallways@users.noreply.github.com>
2023-02-10 22:30:48 +0800

    Update pool.go

--- omega/third_party/sunlife/pool.go
+++ omega/third_party/sunlife/pool.go
@@ -22,5 +22,4 @@ var Components = map[string]defines.Component{
 	"Pvp系统":      &Pvp{BasicComponent: &defines.BasicComponent{}},
 	"自定义附魔升级系统":  &Enchant{BasicComponent: &defines.BasicComponent{}},
 	"无前缀简单自定义指令": &CustomCmd{BasicComponent: &defines.BasicComponent{}},
-	"王国系统":       &King{BasicComponent: &defines.BasicComponent{}},
 }
