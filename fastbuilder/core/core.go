2206318e810279bfc472ea408a1f7f804e69de2a
Eternal Crystal <109064184+Happy2018new@users.noreply.github.com>
2023-05-19 20:34:13 +0800

    fix ConnectionEstablished msg missing (#110)
    Co-authored-by: RainyHallways <78735995+RainyHallways@users.noreply.github.com>

--- fastbuilder/core/core.go
+++ fastbuilder/core/core.go
@@ -176,6 +176,8 @@ func InitClient(env *environment.PBEnvironment) {
 				_, _ = bufio.NewReader(os.Stdin).ReadString('\n')
 			}
 			panic(err)
+		} else {
+		pterm.Println(I18n.T(I18n.ConnectionEstablished))
 		}
 		conn = cconn
 		if len(env.RespondUser) == 0 {
