0bb86a3c7b84339904ae012cd8e49789d45bd5dc
RainyHallways <78735995+RainyHallways@users.noreply.github.com>
2023-07-01 06:17:09 +0800

    Fix bootstrap.go

--- solutions/fastbuilder/bootstrap.go
+++ solutions/fastbuilder/bootstrap.go
@@ -41,7 +41,7 @@ func setup() {
 
 func display_info() {
 	pterm.DefaultBox.Println(pterm.LightCyan("https://github.com/rnhws-Team/PhoenixBuilder"))
-	pterm.Println(pterm.Yellow("PhoenixBuilder Dev " + args.GetFBVersion()))
+	pterm.Println(pterm.Yellow("PhoenixBuilder Dev " + args.FBVersion))
 	if I18n.ShouldDisplaySpecial() {
 		fmt.Printf("%s", I18n.T(I18n.Special_Startup))
 	}
